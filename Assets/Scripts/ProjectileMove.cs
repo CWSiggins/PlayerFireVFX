a1979d374ac8ef07926f380610ec99258e62d645
Cameron Siggins <sigginscameron@gmail.com>
2021-02-21 19:57:18 -0600

    Test Projectile
    First Pass

---
+++ Assets/Scripts/ProjectileMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public float speed;
+    public float fireRate;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (speed != 0)
+        {
+            transform.position += transform.right * (speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("No Speed");
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnProjectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour
+{
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+    public RotateToMouse rotateToMouse;
+
+    private GameObject effectToSpawn;
+
+    void Start()
+    {
+        effectToSpawn = vfx[0];
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SpawnVFX();
+        }   
+    }
+
+    void SpawnVFX()
+    {
+        GameObject vfx;
+        if(firePoint != null)
+        {
+            vfx = Instantiate(effectToSpawn, firePoint.transform.position, Quaternion.identity);
+            if(rotateToMouse != null)
+            {
+                vfx.transform.localRotation = rotateToMouse.GetRotation();
+            }
+        }
+        else
+        {
+            Debug.Log("No Fire Point");
+        }
+    }
+}
